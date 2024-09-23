e47e875f0194a0e72ced40b93959828193da8d52
Andrey <abadan98@mail.ru>
2024-09-23 17:54:09 +0300

    небольшие правки

--- src/components/CartComponents/CartCardItems.tsx
+++ src/components/CartComponents/CartCardItems.tsx
@@ -51,13 +51,16 @@ const CartCarditems = (props: ICartItem) => {
         <>
             <section className="rounded-[30px] mr-0 xs:mr-5 bg-white border w-[300px] sm:w-[500px] border-gray-300 shadow-lg px-5 pb-4 xl:w-[633px]">
                 <div className="flex justify-between">
-                    <div className="flex gap-6 w-[150px] h-[150px]">
-                        <img
-                            className=""
-                            loading="lazy"
-                            src={`/src/assets/${props.item.img}`}
-                            alt="headphone image"
-                        />
+                    <div className="flex gap-6">
+                        <div className="w-[150px] h-[150px]">
+                            <img
+                                className=""
+                                loading="lazy"
+                                src={`/src/assets/${props.item.img}`}
+                                alt="headphone image"
+                            />
+                        </div>
+
                         <div className="flex flex-col justify-center gap-2 ">
                             <div className="text-lg font-medium text-gray-900">
                                 {props.item.title}
